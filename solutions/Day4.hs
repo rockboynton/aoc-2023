5a7636ace918dd273c2ccb06ab84fb851cb7269a
Rock Boynton <rboynton@anduril.com>
2023-12-05 23:43:29 -0800

    day 4 part 2

--- solutions/Day4.hs
+++ solutions/Day4.hs
@@ -9,6 +9,7 @@ data ScratchCard
   = ScratchCard
   { winningNumbers :: [Int]
   , heldNumbers :: [Int]
+  , numCopies :: Int
   }
   deriving (Show)
 
@@ -18,17 +19,30 @@ mkScratchCard bs = do
   let [winningNumbersStr, heldNumbersStr] = BS.split " | " allNums
   let winningNumbers = map (read . B.unpack) (B.words winningNumbersStr)
   let heldNumbers = map (read . B.unpack) (B.words heldNumbersStr)
+  let numCopies = 1
   ScratchCard {..}
 
 scratchCardScore :: ScratchCard -> Int
-scratchCardScore sc = if (not . null) winningNumbersHeld then 2 ^ (length winningNumbersHeld - 1) else 0
+scratchCardScore sc = if (not . null) (winningNumbersHeld sc) then 2 ^ (length (winningNumbersHeld sc) - 1) else 0
+
+winningNumbersHeld :: ScratchCard -> [Int]
+winningNumbersHeld sc = filter (`elem` sc.heldNumbers) sc.winningNumbers
+
+-- copied from mrkline
+moreCards :: [Int] -> [Int] -> [Int]
+moreCards (w:ws) (c:cs) = c : moreCards ws cs'
   where
-    winningNumbersHeld = filter (`elem` sc.heldNumbers) sc.winningNumbers
+    awarded = replicate w c <> repeat 0
+    cs' = zipWith (+) awarded (cs <> repeat 0)
+moreCards _ _ = []
 
 main :: IO ()
 main = do
   contents <- B.readFile "input/day4/input.txt"
   let ls = B.lines contents
-  let cards = map mkScratchCard ls
-  let scores = map scratchCardScore cards
+  let cards = mkScratchCard <$> ls
+  let scores = scratchCardScore <$> cards
   print $ sum scores
+
+  let wins = length . winningNumbersHeld <$> cards
+  print . sum $ moreCards wins (repeat 1)
